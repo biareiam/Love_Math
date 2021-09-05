a8be237b627cc5218b657f14e3d5f01649d7e618
biareiam <biareiam@gmail.com>
2021-09-05 08:09:55 +0000

    adding JavaScript fucntion to the addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,23 +15,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
+
 })
-
-
 
 /**
  * The main game "loop", called when the script is first loaded
  * and afther the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25, it does not include 25. It is rounded (floor)
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting`
+    }
 }
 
 
@@ -51,14 +57,21 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
+
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
 
 }
 
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
