2bcf6924ac5a784e1f8f370d02e31c21c6451110
biareiam <biareiam@gmail.com>
2021-09-05 07:26:49 +0000

    adding function structure to JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function chackAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
